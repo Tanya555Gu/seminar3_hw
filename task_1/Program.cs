541ca6af3a274f80afc103422a3b51bd951fb8a6
Tatyana <tatyana@gus.ru>
2023-04-18 16:39:41 +0300

    Добавление задания task_1 и файла .gitignore

---
+++ task_1/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int ReadInt(string messege)
+{
+    Console.Write($"{messege} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool Validate(int x)
+{
+    if (x < 9999 || x > 100000)
+    {
+        Console.WriteLine("Данное число не является пятизначным");
+        return false;
+    }
+    return true;
+}
+
+string BePalind(int number)
+{
+    int a1 = number / 10000 % 10;
+    int a2 = number / 1000 % 10;
+    int a4 = number / 10 % 10;
+    int a5 = number % 10;
+    if (a1 == a5 && a2 == a4)
+    {
+        Console.Write($"Число {number} ");
+    }
+    else
+    {
+        Console.Write($"Число {number} не ");
+    }
+    return $"является полиндромом";
+}
+
+int n = ReadInt("Введите пятизначное число");
+if (Validate(n))
+{
+    Console.WriteLine(BePalind(n));
+}
+
